f7e5292a6464a83eeb62110ff1686a4aed212135
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-10-10 21:01:45 -0400

    Created project and Photo model

---
+++ PhotoCollection/PhotoCollection/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  PhotoCollection
+//
+//  Created by Christy Hicks on 10/10/19.
+//  Copyright © 2019 Knight Night. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
---
+++ PhotoCollection/PhotoCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoCollection
+//
+//  Created by Christy Hicks on 10/10/19.
+//  Copyright © 2019 Knight Night. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
